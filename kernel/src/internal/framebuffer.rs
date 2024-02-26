d01b6d0ab6dfee5fb5e15475c84fa2e149a273b6
Lukas Küffer <lukas.kueffer@outlook.com>
2024-02-26 22:10:06 +0100

    Added access functions to internal framebuffer module

--- kernel/src/internal/framebuffer.rs
+++ kernel/src/internal/framebuffer.rs
@@ -1,12 +1,31 @@
 use alloc::sync::Arc;
 use bootloader_api::info::FrameBufferInfo;
-use spin::lock_api::Mutex;
-use spin::Once;
+use spin::{Mutex, Once};
 
 static FRAMEBUFFER: Once<Arc<Mutex<&'static mut [u8]>>> = Once::new();
 static FRAMEBUFFER_INFO: Once<FrameBufferInfo> = Once::new();
 
-pub fn init(framebuffer: &'static mut [u8], framebuffer_info: FrameBufferInfo) {
-    FRAMEBUFFER.call_once(|| Arc::new(Mutex::new(framebuffer)));
-    FRAMEBUFFER_INFO.call_once(|| framebuffer_info);
+pub struct Framebuffer {
+    buffer: Arc<Mutex<&'static mut [u8]>>,
+    info: FrameBufferInfo,
+} impl Framebuffer {
+    pub(crate) fn global() -> Option<Self> {
+        Some(Self {
+            buffer: FRAMEBUFFER.get()?.clone(),
+            info: *FRAMEBUFFER_INFO.get()?,
+        })
+    }
+
+    pub fn consume<F, T>(&self, func: F) -> Option<T>
+        where F: FnOnce(&mut [u8], FrameBufferInfo) -> T, {
+
+        if let Some(mut buffer) = self.buffer.try_lock() {
+            Some(func(&mut buffer, self.info))
+        } else { None }
+    }
+}
+
+pub fn init(buffer: &'static mut [u8], info: FrameBufferInfo) {
+    FRAMEBUFFER.call_once(|| Arc::new(Mutex::new(buffer)));
+    FRAMEBUFFER_INFO.call_once(|| info);
 }
